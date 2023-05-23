0e86ab1723b8601561ed997541b26badf23a3019
EC2 Default User <ec2-user@ip-172-31-16-39.ap-south-2.compute.internal>
2023-05-23 10:37:02 +0000

    test

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello world from dockerized flaskk'
+    return 'Hello world from dockerized flaskkk'
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)
